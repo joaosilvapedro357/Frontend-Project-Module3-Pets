c9e248427bbf4c2106afedee8daea2f7ad315deb
João Silva <joaosilvapedro357@gmail.com>
2023-12-06 11:01:12 +0000

    css minor updates

--- react-upet-app/src/pages/EditPetPage.jsx
+++ react-upet-app/src/pages/EditPetPage.jsx
@@ -81,57 +81,89 @@ function EditPetPage() {
 
     return(
         <div>
-            <form onSubmit={handleSubmit}>
-                <label>Name:
-                  <input type="text" name="name" value={name} onChange={(e)=> 
-                    setName(e.target.value)}/>
-                </label>
-                <label>Image:
-                  <input type="text" name="image" value={image} onChange={(e)=> 
-                    setImage(e.target.value)}/>
-                </label>
-                <label>Age:
-                  <input type="text" name="age" value={age} onChange={(e)=> 
-                    setAge(e.target.value)}/>
-                </label>
-                <label>Breed:
-                  <input type="text" name="breed" value={breed} onChange={(e)=> 
-                    setBreed(e.target.value)}/>
-                </label>
-                <label>Hair Type:
-                  <input type="text" name="hairType" value={hairType} onChange={(e)=> 
-                    setHairType(e.target.value)}/>
-                </label>
-                <label>Chip Id:
-                  <input type="text" name="chipId" value={chipId} onChange={(e)=> 
-                    setChipId(e.target.value)}/>
-                </label>
-                <label>Sex:
-                  <input type="text" name="sex" value={sex} onChange={(e)=> 
-                    setSex(e.target.value)}/>
-                </label>
-                <label>Size:
-                  <input type="text" name="size" value={size} onChange={(e)=> 
-                    setSize(e.target.value)}/>
-                </label>
-                <label>Weight:
-                  <input type="text" name="weight" value={weight} onChange={(e)=> 
-                    setWeight(e.target.value)}/>
-                </label>
-                <label>Description:
-                  <input type="text" name="description" value={description} 
-                  onChange={(e)=> setDescription(e.target.value)}/>
-                </label>
-                <label>Diet:
-                  <input type="text" name="diet" value={diet} onChange={(e)=> 
-                    setDiet(e.target.value)}/>
-                </label>
-                <label>Medical Record:
-                  <input type="text" name="medicalRecord" value={medicalRecord} 
-                  onChange={(e)=> setMedicalRecord(e.target.value)}/>
-                </label>
-                <button type="submit">Edit Pet</button>
-            </form>
+            <form onSubmit={handleSubmit} className="pets-create-form">
+      <div className="creatediv-column">
+        <div className="pets-create">
+        <div className="pet-create1">
+          <label className="form-pet-name">
+            <p className="p-form-name"> Name: </p>
+            <input className="form-name" type="text" name="name" value={name} 
+            onChange={(e)=>setName(e.target.value)}/>
+          </label>
+          <label className="form-pet-image">
+            <p className="p-form-image"> Image: </p>
+            <input className="form-image" type="text" name="image" value={image} 
+            onChange={(e) => setImage(e.target.value)}/>
+          </label>
+          <label className="form-pet-age">
+            <p className="p-form-age"> Age: </p>
+            <input className="form-age" type="text" name="age" value={age} 
+            onChange={(e) => setAge(e.target.value)}/>
+          </label>
+          <label className="form-pet-breed">
+            <p className="p-form-breed"> Breed: </p>
+            <input className="form-breed" type="text" name="breed" value={breed} 
+            onChange={(e) => setBreed(e.target.value)}/>
+          </label>
+          <label className="form-pet-hair">
+            <p className="p-form-hair"> Hair: </p>
+            <input className="form-hair" type="text" name="hairType" value={hairType} 
+            onChange={(e) => setHairType(e.target.value)}/>
+          </label>
+          <label className="form-pet-chip">
+            <p className="p-form-chip"> Chip: </p>
+            <input className="form-chip" type="text" name="chipId" value={chipId} 
+            onChange={(e) => setChipId(e.target.value)}/>
+          </label> 
+          <br/> 
+        </div>
+        <div className="pet-create2">
+          <label className="form-pet-sex">
+            <p className="p-form-sex"> Sex: </p>
+            <input className="form-sex" type="text" name="sex" value={sex} 
+            onChange={(e) => setSex(e.target.value)}/>
+          </label>
+          <label className="form-pet-size">
+            <p className="p-form-size"> Size: </p>
+            <input className="form-size" type="text" name="size" value={size} 
+            onChange={(e) => setSize(e.target.value)}/>
+          </label>
+          <label className="form-pet-weight">
+            <p className="p-form-weight"> Weight: </p>
+            <input className="form-weight" type="text" name="weight" value={weight} 
+            onChange={(e) => setWeight(e.target.value)}/>
+          </label>
+          <label className="form-pet-description">
+            <p className="p-form-description"> Description: </p>
+            <input className="form-description" type="text" name="description" 
+            value={description} onChange={(e) => setDescription(e.target.value)}/>
+          </label>
+          <label className="form-pet-diet">
+            <p className="p-form-diet"> Diet: </p>
+            <input className="form-diet" type="text" name="diet" value={diet} 
+            onChange={(e) => setDiet(e.target.value)}/>
+          </label>
+          <label className="form-pet-medical">
+            <p className="p-form-medical"> Medical Record: </p>
+            <input className="form-medical" type="text" name="medicalRecord" 
+            value={medicalRecord} onChange={(e) => setMedicalRecord(e.target.value)}/>
+          </label>
+          <label className="form-pet-medical">
+            <p className="p-form-medical"> Type of Pet:</p>
+            <select value={typeOfPet} onChange={(e) => 
+              setTypeOfPet(e.target.value)}>
+                <option>Dog</option>
+                <option>Cat</option>
+                <option>Other Pet</option>
+            </select>
+          </label>
+        </div>
+        </div>
+        <div className="add-buton">
+          <button type="submit">Edit Pet</button>
+        </div>
+        </div>
+      </form>
             <button onClick={deletePet}>Delete</button>
         </div>
     )
